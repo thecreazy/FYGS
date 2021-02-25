79026c44bd931186580329de5e6286d113b1d418
Riccardo Canella <canella_riccardo@icloud.com>
2021-02-25 08:50:10 +0100

    feat(core): commit single file and not all [M file: script.sh] script.sh

--- script.sh
+++ script.sh
@@ -3,7 +3,6 @@
 #COSTANTS
 RED='\033[0;31m'
 GREEN='\033[0;32m'
-BLUE='\033[0;36m'
 NC='\033[0m'
 
 helpFunction()
@@ -27,14 +26,12 @@ then
    helpFunction
 fi
 
-echo $commitMessage
-
 editedOutput=$(git status -s | grep M)
 splittedEdited=$(echo $editedOutput | tr "M " "\n")
 for singleEdited in $splittedEdited
 do
     git add $singleEdited
-    git commit -a -m"$commitMessage [M file: $singleEdited]"
+    git commit -m"$commitMessage [M file: $singleEdited] $singleEdited"
 done
 
 deletedOutput=$(git status -s | grep D)
@@ -42,7 +39,7 @@ splittedDeleted=$(echo $deletedOutput | tr "D " "\n")
 for singleDeleted in $splittedDeleted
 do
     git add $singleDeleted
-    git commit -a -m"$commitMessage [D file: $singleDeleted]"
+    git commit -m"$commitMessage [D file: $singleDeleted] $singleDeleted"
 done
 
 untrackedOutput=$(git status -s | grep ??)
@@ -50,9 +47,10 @@ splittedUntracked=$(echo $untrackedOutput | tr "?? " "\n")
 for singleUntracked in $splittedUntracked
 do
     git add $singleUntracked
-    git commit -a -m"$commitMessage [?? file: $singleUntracked]"
+    git commit -m"$commitMessage [?? file: $singleUntracked] $singleUntracked"
 done
 
+printf "${GREEN}commit: $commitMessage done :) enjoy your pumped stats${NC}";
 
 
 
